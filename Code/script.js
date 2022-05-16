8041a1edbe6abed022fef0af8f8737aa624c6606
NathanStein1 <nathanstein2@gmail.com>
2022-05-16 09:41:18 -0700

    transitions work

--- Code/script.js
+++ Code/script.js
@@ -13,23 +13,41 @@ let conB = document.querySelector('.conB')
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-conB.onclick = function() {
+conB.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
+let about = document.querySelector('.aboot')
+let work = document.querySelector('.werk')
+let gallery = document.querySelector('.gallery')
+let footer = document.querySelector('.footy')
+let wrap = document.querySelector('.wrapper')
 
+
+about.addEventListener('click', function () {
+
+  footer.classList.remove('bottom')
+  footer.classList.add('fixed-bottom')
+
+})
+work.addEventListener('click', function () {
+
+  footer.classList.remove('fixed-bottom')
+  footer.classList.add('bottom')
+
+})
 
 
 

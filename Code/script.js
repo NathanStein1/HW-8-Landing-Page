418e8fb40d0335aa9147cfb5365a046058c5a740
NathanStein1 <nathanstein2@gmail.com>
2022-05-16 09:10:51 -0700

    node add

--- Code/script.js
+++ Code/script.js
@@ -1,5 +1,9 @@
+// import Swup from 'swup'
+const swup = new Swup()
 
 
+
+// CODE FOR MODAL
 let modal = document.querySelector('.modal')
 let conB = document.querySelector('.conB')
 
@@ -26,33 +30,8 @@ window.onclick = function(event) {
 }
 
 
-// EVENT LISTENER FOR WORK AND ABOUT ME SECTION
-
-let about = document.querySelector('.aboot')
-let work = document.querySelector('.werk')
-let gallery = document.querySelector('.gallery')
-let footer = document.querySelector('.footy')
-let wrap = document.querySelector('.wrapper')
 
 
-
-about.addEventListener('click', function(){
-gallery.classList.add('hide')
-footer.classList.remove('bottom')
-footer.classList.add('fixed-bottom')
-if (wrap.classList.contains('hide')){
-  wrap.classList.remove('hide')
-
-}
-})
-
-work.addEventListener('click', function(){
-gallery.classList.remove('hide')
-wrap.classList.add('hide')
-footer.classList.remove('fixed-bottom')
-footer.classList.add('bottom')
-
-})
 
 // On hover, set opacity to .8 in css. hover item1, event listener('hover'). .style.opacity = .8;
 
